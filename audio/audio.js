b7ef963c0018679fbe18405ec02d39d08a8a7ac4
Guilhermekaiserdealmeida <128114151+Guilhermekaiserdealmeida@users.noreply.github.com>
2024-07-01 12:23:45 -0300

    Add files via upload

---
+++ audio/audio.js
@@ -0,0 +1,53 @@
+var som1 = new Audio();
+som1.src = "a.mp3";
+
+var som2 = new Audio();
+som2.src = "b.mp3";
+
+var som3 = new Audio();
+som3.src = "c.mp3";
+
+var som4 = new Audio();
+som4.src = "d.mp3";
+
+var som5 = new Audio();
+som5.src = "e.mp3";
+
+var som6 = new Audio();
+som6.src = "f.mp3";
+
+var som7 = new Audio();
+som7.src = "g.mp3";
+
+var som8 = new Audio();
+som8.src = "h.mp3";
+
+var som9 = new Audio();
+som9.src = "i.mp3";
+
+var som10 = new Audio();
+som10.src = "j.mp3";
+
+var som11 = new Audio();
+som11.src = "k.mp3";
+
+var som12 = new Audio();
+som12.src = "l.mp3";
+
+var som13 = new Audio();
+som13.src = "m.mp3";
+
+var som14 = new Audio();
+som14.src = "n.mp3";
+
+var som15 = new Audio();
+som15.src = "o.mp3";
+
+var som16 = new Audio();
+som16.src = "p.mp3";
+
+var som17 = new Audio();
+som17.src = "q.mp3";
+
+var som18 = new Audio();
+som18.src = "r.mp3";
